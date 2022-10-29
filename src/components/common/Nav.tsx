d71b6e1414e73c320b322842473aeebe7a8307c9
AnthonyGonzalez7 <anthony.gonzalez7@utp.ac.pa>
2022-10-29 15:21:46 -0500

    v3

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -9,7 +9,7 @@ export const Nav:FC = () =>{
         <nav className="px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <Link href="/" className="flex items-center">
-                <img src="/images/logo.png" className="mr-3 h-6 sm:h-9" alt="Logo Automoviles"></img>
+                <img src='/images/logo.png' className="mr-3 h-6 sm:h-9" alt="Logo Automoviles" />
                 </Link>
                 <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
