e758fe3af9ed5802f0537aa89a78a23c8c64b929
AnthonyGonzalez7 <anthony.gonzalez7@utp.ac.pa>
2022-10-29 15:33:06 -0500

    v3

--- src/components/layout/ApplicationWrapper.tsx
+++ src/components/layout/ApplicationWrapper.tsx
@@ -18,8 +18,8 @@ export const ApplicationWrapper: FC<PropsWithChildren <Tprops>> = ({
         <Head>
             <title>{title} | Autos Panamá</title>
             {description && <meta name="description" content={description} />}
-            <Link rel="icon" href="/images/logo.png" />
-            <Link
+            <link rel="icon" href="/images/logo.png" />
+            <link
               rel="stylesheet"
               href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
             />
